3cf7c2b3f23c2131585c2532ba36c9d0081fa296
Hersonrock <hjpg20@gmail.com>
2025-01-07 16:34:54 -0500

    Adds color buffer basic definition and setup function for it

--- 3drenderer/main.c
+++ 3drenderer/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <SDL.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 is_running = false;
+uint32_t* color_buffer = NULL;
+
 
 bool initialize_window(void) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -16,8 +23,8 @@ bool initialize_window(void) {
 		NULL,
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
 		SDL_WINDOW_BORDERLESS
 	);
 
@@ -37,7 +44,19 @@ bool initialize_window(void) {
 }
 
 void setup(void) {
+	color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * WINDOW_WIDTH * WINDOW_HEIGHT);
 
+	if (!color_buffer) {
+		fprintf(stderr, "Failed to allocate color buffer.\n");
+	}
+}
+
+void destroy_window(void) {
+
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 void process_input(void) {
@@ -84,6 +103,6 @@ int main(int argc, char* argv[]) {
 		update();
 		render();
 	}
-
+	destroy_window();
 	return 0;
 }
