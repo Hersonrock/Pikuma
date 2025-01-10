1c4eeee93d13647ddf81eb15ecc22fecb313b51e
Hersonrock <hjpg20@gmail.com>
2025-01-10 11:10:36 -0500

    Adding vector files

---
+++ 3drenderer/vector.h
@@ -0,0 +1,17 @@
+#ifndef VECTOR
+#define VECTOR
+
+typedef struct {
+	float x;
+	float y;
+}vect2_t;
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+}vect3_t;
+
+// TODO: Add functions to manipulate vectors
+
+#endif #VECTOR
