8ae238c7ecd9fcbacba57fb1235eacd9e95dfb50
Hersonrock <hjpg20@gmail.com>
2025-01-22 19:54:18 -0500

    Adding Vector operations, Norm and addition

--- 3drenderer/vector.c
+++ 3drenderer/vector.c
@@ -28,3 +28,33 @@ vect3_t vec3_rotate_z(vect3_t v, float angle) {
 
 	return rotated_vector;
 }
+
+
+/////////////////////VECTOR 2D//////////////////
+float vect2_lenght(vect2_t v){
+        float lenght = v.x * v.x + v.y * v.y;
+        lenght = sqrt(lenght);
+        return lenght;
+}
+
+vect2_t vect2_add(vect2_t v1, vect2_t v2){
+        vect2_t result;
+        result.x = v1.x + v2.x;
+        result.y = v1.y + v2.y;
+
+        return result;
+}
+/////////////////////VECTOR 3D//////////////////
+float vect3_lenght(vect3_t v){
+        float lenght = v.x * v.x + v.y * v.y + v.z * v.z;
+        lenght = sqrt(lenght);
+        return lenght;
+}
+vect3_t vect3_add(vect3_t v1, vect3_t v2){
+        vect3_t result;
+        result.x = v1.x + v2.x;
+        result.y = v1.y + v2.y;
+        result.z = v1.z + v2.z;
+
+        return result;
+}
--- 3drenderer/vector.h
+++ 3drenderer/vector.h
@@ -15,5 +15,11 @@ typedef struct {
 vect3_t vec3_rotate_x(vect3_t v, float angle);
 vect3_t vec3_rotate_y(vect3_t v, float angle);
 vect3_t vec3_rotate_z(vect3_t v, float angle);
+/////////////////////VECTOR 2D//////////////////
+float vect2_lenght(vect2_t v);
+vect2_t vect2_add(vect2_t v1, vect2_t v2);
+/////////////////////VECTOR 3D//////////////////
+float vect3_lenght(vect3_t v);
+vect3_t vect3_add(vect3_t v1, vect3_t v2);
 
 #endif //VECTOR
