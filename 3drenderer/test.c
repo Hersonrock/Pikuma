a213ca305588d61c776941accde2a3710a020ff8
Hersonrock <hjpg20@gmail.com>
2025-01-24 17:04:33 -0500

    Working basic unit tests for lenght

--- 3drenderer/test.c
+++ 3drenderer/test.c
@@ -1,4 +1,16 @@
 #include "test.h"
 
-
+void test_vect2_lenght(){
+        vect2_t v1 = { .x = 3, .y = 4 };
+        float norm = vect2_lenght(v1);
+        assert(fabs(norm - 5) < TOLERANCE);
+        printf("test_vect2_lenght passed!\n");
+}
+void test_vect3_lenght(){
+        vect3_t v1 = { .x = 3, .y = 4, .z = 6};
+        float norm = vect3_lenght(v1);
+        float expected = 7.81f;
+        assert(fabs(norm - expected) < TOLERANCE);
+        printf("test_vect3_lenght passed!\n");
+}
 //TODO functions to tests 
--- 3drenderer/test.h
+++ 3drenderer/test.h
@@ -1,4 +1,11 @@
 #include <assert.h>
+#include <float.h>
+#include <math.h>
 #include "vector.h"
 
+#define TOLERANCE  0.001
+
 //TODO: Functions protoypes for tests
+
+void test_vect2_lenght();
+void test_vect3_lenght();
